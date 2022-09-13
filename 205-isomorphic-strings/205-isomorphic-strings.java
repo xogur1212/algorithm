de6b28482e2045bda2b7cf3c3fe3ba979f46c70d
xy124 <84655881+xogur1212@users.noreply.github.com>
2022-09-13 15:48:04 +0900

    Time: 97 ms (5.46%), Space: 51.5 MB (5.04%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,50 @@
+class Solution {
+    public  boolean isIsomorphic(String s, String t) {
+
+        String [] sSplit =s.split("");
+
+        String [] tSplit =t.split("");
+
+        int size = sSplit.length;
+        Map<String,String> isIsomorphicMap =new HashMap<>();
+        Map<String,String> reverseMap =new HashMap<>();
+
+        for(int count= 0; count <= size-1; count ++)  {
+            isIsomorphicMap.put(sSplit[count],tSplit[count]);
+            reverseMap.put(tSplit[count],sSplit[count]);
+
+
+        }
+        for(int count =0;count <= size-1; count ++){
+
+                if(isIsomorphicMap.containsKey(sSplit[count])){
+
+                    if(!isIsomorphicMap.get(sSplit[count]).equals(tSplit[count])){
+
+                        return false;
+                    }
+                }
+
+        }
+
+
+
+        for(int count =0;count <= size-1; count ++){
+
+            if(reverseMap.containsKey(tSplit[count])){
+
+                if(!reverseMap.get(tSplit[count]).equals(sSplit[count])){
+
+                    return false;
+                }
+            }
+
+
+        }
+
+
+
+        return true;
+
+    }
+}
