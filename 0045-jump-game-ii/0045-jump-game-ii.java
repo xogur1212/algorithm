222bbfc5ea82e02275f6e137465fece9ac37de14
xy124 <84655881+xogur1212@users.noreply.github.com>
2023-11-04 21:24:55 +0900

    Time: 53 ms (14.18%), Space: 45.1 MB (8.73%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -5,6 +5,7 @@ class Solution {
         Queue<Integer> queue = new ArrayDeque<>();
         queue.add(0);
         int jumps = 0;
+        boolean[] visited = new boolean[arrayLength];
 
         while (!queue.isEmpty()) {
             int size = queue.size();
@@ -12,18 +13,18 @@ class Solution {
             for (int count = 0; count < size; count++) {
                 int current = queue.remove();
                 if (current == arrayLength - 1) return jumps;
-                if(nums[current] == -1) continue;
                 for (int i = current + 1; i <= Math.min(arrayLength - 1, current + nums[current]); i++) {
-                    if (nums[i] != -1 ) {
+                    if (!visited[i]) {
                         queue.add(i);
-
+                        visited[i] =true;
                     }
                 }
-                nums[current] = -1;
+                visited[current] = true;
             }
             jumps++;
         }
         return -1;
     }
+
 
 }
